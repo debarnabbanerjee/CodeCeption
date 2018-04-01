75f1c5f393713b0d5bcf6bbb760b7a66796af27a
Debarnab Banerjee <banerjee.debarnab@gmail.com>
2018-04-01 15:09:13 +0200

    Splitted the Reservation Feature into 2 tests

--- reservation_test.js
+++ reservation_test.js
@@ -1,7 +1,7 @@
 
-Feature('To Test the Reservation Page Functionality');
+Feature('Reservation Page Tests');
 
-Scenario('Test the proper UI of the Reservation Page', async (I, loginPage, calendarPage, reservationPage) => {
+Scenario('Reservation Page UI', async (I, loginPage, calendarPage, reservationPage) => {
      I.amOnPage('/');
 	 loginPage.doLogin('banerjed','California0!');
 	 I.wait(2);
@@ -9,33 +9,38 @@ Scenario('Test the proper UI of the Reservation Page', async (I, loginPage, cale
 	 calendarPage.clickReservation();
      I.wait(2);
 	 I.seeTitleEquals('Reservations - base7');
-
-	 var number = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[1]');
-	 var client = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[2]');
-	 var dates = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[3]');
-	 var created = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[4]');
-	 var source = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[5]');
-	 var room = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[6]');
-	 var prereservation = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[7]/div');
-
-     console.log('number is ' + number);
-     console.log('client is ' + client);
-     console.log('dates is ' + dates);
-     console.log('created is ' + created);
-     console.log('source is ' + source);
-     console.log('roomType is ' + room);
-	 console.log('prereservation is ' + prereservation);
-
 	 reservationPage.verifyReservationPageUI();
-	 // reservationPage.clickOnFirstReservationItem();
-	 //I.click('.flex > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1)');
-     // I.waitForElement('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span/b', 2);
-     //
-     // var clientText = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span/b');
-     // console.log('Client Text is ' + clientText);
-     // I.see(clientText, {xpath: '/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span/b'});
-     //
-     // var roomType = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[6]/div[3]/div[2]/table/tbody/tr[1]/td[1]');
-	 // I.see(roomType, {xpath: '/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[6]/div[3]/div[2]/table/tbody/tr[1]/td[1]'});
+});
+
+Scenario('Test the functionality of the Latest Reservation Item', async (I, loginPage, calendarPage, reservationPage) => {
+
+    var number = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[1]');
+    var client = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[2]');
+    var dates = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[3]');
+    var created = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[4]');
+    var source = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[5]');
+    var room = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[6]');
+    var prereservation = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/table/tbody/tr[3]/td[7]/div');
+
+    //I.addMochawesomeContext('simple string');
+    console.log('client is ' + client);
+    console.log('dates is ' + dates);
+    console.log('created is ' + created);
+    console.log('source is ' + source);
+    console.log('roomType is ' + room);
+    console.log('prereservation is ' + prereservation);
+
+     reservationPage.clickOnFirstReservationItem();
+
+    //I.click('.flex > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1)');
+    // I.waitForElement('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span/b', 2);
+    //
+    // var clientText = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span/b');
+    // console.log('Client Text is ' + clientText);
+    // I.see(clientText, {xpath: '/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span/b'});
+    //
+    // var roomType = await I.grabTextFrom('/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[6]/div[3]/div[2]/table/tbody/tr[1]/td[1]');
+    // I.see(roomType, {xpath: '/html/body/div[1]/div/div[5]/div/div[1]/div[3]/div[6]/div[3]/div[2]/table/tbody/tr[1]/td[1]'});
 
 });
+
